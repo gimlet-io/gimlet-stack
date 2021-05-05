a4be98e2b1f94753bd62911190f5f93449704244
Laszlo Fogas <laszlo@laszlo.cloud>
2021-05-05 10:23:54 +0200

    stack generate

--- commands/generate.go
+++ commands/generate.go
@@ -1,7 +1,13 @@
 package commands
 
 import (
+	"fmt"
+	"github.com/gimlet-io/gimlet-stack/template"
 	"github.com/urfave/cli/v2"
+	"gopkg.in/yaml.v3"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 )
 
 var GenerateCmd = cli.Command{
@@ -10,9 +16,51 @@ var GenerateCmd = cli.Command{
 	UsageText: `stack generate`,
 	Action:    generate,
 	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:     "config",
+			Aliases:  []string{"c"},
+		},
+		&cli.StringFlag{
+			Name:     "target-path",
+			Aliases:  []string{"p"},
+		},
 	},
 }
 
 func generate(c *cli.Context) error {
+
+	stackConfigPath := c.String("config")
+	if stackConfigPath == "" {
+		stackConfigPath = "stack.yaml"
+	}
+	stackConfigYaml, err := ioutil.ReadFile(stackConfigPath)
+	if err != nil {
+		return fmt.Errorf("cannot read stack config file: %s", err.Error())
+	}
+
+	var stackConfig template.StackConfig
+	err = yaml.Unmarshal([]byte(stackConfigYaml), &stackConfig)
+	if err != nil {
+		return fmt.Errorf("cannot parse stack config file: %s", err.Error())
+	}
+
+	files, err := template.GenerateFromStackYaml(stackConfig)
+	if err != nil {
+		return fmt.Errorf("cannot parse stack config file: %s", err.Error())
+	}
+
+	targetPath := c.String("target-path")
+	for path, content := range files {
+		err := os.MkdirAll(filepath.Dir(path), 0775)
+		if err != nil {
+			return fmt.Errorf("cannot write stack: %s", err.Error())
+		}
+
+		err = ioutil.WriteFile(filepath.Join(targetPath, path), []byte(content), 0664)
+		if err != nil {
+			return fmt.Errorf("cannot write stack: %s", err.Error())
+		}
+	}
+
 	return nil
 }
