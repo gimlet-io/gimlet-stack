a3574ccf9169cd9a93b665a31ac4351b77deac4c
laszlo <laszlo@laszlo.cloud>
2021-05-07 11:53:59 +0200

    Tabs

--- web/src/app.js
+++ web/src/app.js
@@ -118,6 +118,7 @@ class App extends Component {
             let selectedComponentConfig = undefined;
             let componentSaver = undefined;
             const selectedComponentName = toggleState[category.id];
+
             if (selectedComponentName !== undefined) {
               const selectedComponentArray = stackDefinition.components.filter(component => component.variable === toggleState[category.id]);
               selectedComponent = selectedComponentArray[0];
@@ -144,28 +145,59 @@ class App extends Component {
             })
 
             const componentConfigPanel = selectedComponentName === undefined ? null : (
-              <div>
-                <div className="shadow sm:rounded-md sm:overflow-hidden">
-                  <div className="bg-white py-6 px-4 space-y-6 sm:p-6">
-                    <HelmUI
-                      schema={selectedComponent.schema}
-                      config={selectedComponent.uiSchema}
-                      values={selectedComponentConfig}
-                      setValues={componentSaver}
-                    />
-                  </div>
-                </div>
+              <div className="py-6 px-4 space-y-6 sm:p-6">
+                <HelmUI
+                  schema={selectedComponent.schema}
+                  config={selectedComponent.uiSchema}
+                  values={selectedComponentConfig}
+                  setValues={componentSaver}
+                />
               </div>
             );
 
             return (
               <div className="container mx-auto m-8 max-w-4xl">
                 <h2>{category.name}</h2>
-                <div className="flex space-x-2">
+                <div className="flex space-x-2 my-2">
                   {componentTitles}
                 </div>
                 <div className="my-2">
-                  {componentConfigPanel}
+                  { selectedComponentName !== undefined &&
+                  <div className="px-8 py-4 shadow sm:rounded-md sm:overflow-hidden bg-white">
+                    <div>
+                      <div className="sm:hidden">
+                        <label htmlFor="tabs" className="sr-only">Select a tab</label>
+                        <select id="tabs" name="tabs"
+                                className="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
+                          <option selected>Values</option>
+                          <option>Info</option>
+                          <option>Getting Started</option>
+                        </select>
+                      </div>
+                      <div className="hidden sm:block">
+                        <div className="border-b border-gray-200">
+                          <nav className="-mb-px flex space-x-8" aria-label="Tabs">
+                            <a href="#"
+                               className="border-indigo-500 text-indigo-600 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm"
+                               aria-current="page"
+                            >
+                              Values
+                            </a>
+                            <a href="#"
+                               className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm">
+                              Info
+                            </a>
+                            <a href="#"
+                               className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm">
+                              Getting Started
+                            </a>
+                          </nav>
+                        </div>
+                      </div>
+                    </div>
+                    {componentConfigPanel}
+                  </div>
+                  }
                 </div>
               </div>
             )
